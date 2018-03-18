1cdd25d7cea194b03ebea7b3579d94d204902828
Alex Bennett <benedictus.ab@gmail.com>
2018-03-18 09:02:47 -0600

    moving obstacle/end game if crash

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# SillyBird
->>>>>>> e529a9a61cfed53cab2f5cb29980d4434805ea77
+SillyBird
 
 Flappy Bird clone with different changes/features.
--- index.js
+++ index.js
@@ -5,8 +5,9 @@ function startGame() {
 var myGamePiece;
 
 function startGame() {
-  myGameArea.start();
   myGamePiece = new component(30, 30, "green", 10, 120);
+  myObstacle = new component(10, 200, "maroon", 300, 120);
+  myGameArea.start();
 }
 
 var myGameArea = {
@@ -27,37 +28,76 @@ var myGameArea = {
   },
   clear : function() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+  stop : function() {
+    clearInterval(this.interval);
   }
 }
 
-function component(width, height, color, x, y) {
-    this.width = width;
-    this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;
-    this.x = x;
-    this.y = y;
-    this.update = function() {
-      ctx = myGameArea.context;
+function component(width, height, color, x, y, type) {
+  this.type = type;
+  if (type == "image") {
+    this.image = new Image();
+    this.image.src = color;
+  }
+  this.width = width;
+  this.height = height;
+  this.speedX = 0;
+  this.speedY = 0;
+  this.x = x;
+  this.y = y;
+  this.update = function() {
+    ctx = myGameArea.context;
+    if (type == "image") {
+      ctx.drawImage(this.image,
+      this.x,
+      this.y,
+      this.width, this.height);
+    } else {
       ctx.fillStyle = color;
       ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-    this.newPos = function() {
-      this.x += this.speedX;
-      this.y += this.speedY;
-    }
+  }
+  this.newPos = function() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+  this.crashWith = function(otherobj) {
+    var myleft = this.x;
+    var myright = this.x + (this.width);
+    var mytop = this.y;
+    var mybottom = this.y + (this.height);
+    var otherleft = otherobj.x;
+    var otherright = otherobj.x + (otherobj.width);
+    var othertop = otherobj.y;
+    var otherbottom = otherobj.y + (otherobj.height);
+    var crash = true;
+    if ((mybottom < othertop) ||
+          (mytop > otherbottom) ||
+          (myright < otherleft) ||
+          (myleft > otherright)) {
+        crash = false;
+      }
+      return crash;
+  }
 }
 
 function updateGameArea() {
-  myGameArea.clear();
-  myGamePiece.speedX = 0;
-  myGamePiece.speedY = 0;
-  if (myGameArea.keys && myGameArea.keys[37]) {myGamePiece.speedX = -2; }
-  if (myGameArea.keys && myGameArea.keys[39]) {myGamePiece.speedX = 2; }
-  if (myGameArea.keys && myGameArea.keys[38]) {myGamePiece.speedY = -2; }
-  if (myGameArea.keys && myGameArea.keys[40]) {myGamePiece.speedY = 2; }
-  myGamePiece.newPos();
-  myGamePiece.update();
+  if (myGamePiece.crashWith(myObstacle)) {
+    myGameArea.stop();
+  } else {
+    myGameArea.clear();
+    myObstacle.x += -1;
+    myObstacle.update();
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;
+    if (myGameArea.keys && myGameArea.keys[37]) {myGamePiece.speedX = -2; }
+    if (myGameArea.keys && myGameArea.keys[39]) {myGamePiece.speedX = 2; }
+    if (myGameArea.keys && myGameArea.keys[38]) {myGamePiece.speedY = -2; }
+    if (myGameArea.keys && myGameArea.keys[40]) {myGamePiece.speedY = 2; }
+    myGamePiece.newPos();
+    myGamePiece.update();
+  }
 }
 
 function moveUp() {
